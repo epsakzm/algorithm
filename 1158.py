9b4abb9021912c41327c7029f4afc12ec0cef9c3
epsakzm <parkhw0614@gmail.com>
2020-07-05 17:10:45 +0900

    python

---
+++ 1158.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+N, K = map(int, input().split())
+
+people = list(range(1,N+1))
+result = []
+i = K-1
+while True:
+    result.append(people.pop(i))
+    if not people:
+        break
+    i = (i+k-1) % len(people)
+print('<'+','.join(map(str,result))+'>')
