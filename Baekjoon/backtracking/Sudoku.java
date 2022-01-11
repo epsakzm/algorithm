483a8268699dc759831ba03336f5634659ebdd2b
박형우 <parkhw0614@gmail.com>
2022-01-12 01:04:58 +0900

    backtracking & dfs

---
+++ Baekjoon/backtracking/Sudoku.java
@@ -0,0 +1,80 @@
+package Baekjoon.backtracking;
+
+import java.util.Scanner;
+
+public class Sudoku {
+
+    static StringBuilder sb = new StringBuilder();
+    static int[][] map = new int[9][9];
+    static Pair[] pairs = new Pair[81];
+    static int len = 0;
+
+    private static void dfs(int depth) {
+        if (depth == len) {
+            for (int[] x : map) {
+                for (int y : x)
+                    sb.append(y).append(" ");
+                sb.append("\n");
+            }
+            System.out.println(sb.toString());
+            System.exit(0);
+        }
+
+        int x = pairs[depth].x;
+        int y = pairs[depth].y;
+
+        for (int num = 1; num <= 9; ++num)
+            if (check(x, y, num)) {
+                map[x][y] = num;
+                dfs(depth + 1);
+                map[x][y] = 0;
+            }
+    }
+
+    private static boolean check(int r, int c, int target) {
+        // 행열 체크
+        for (int idx = 0; idx < 9; ++idx) {
+            if (map[r][idx] == target)
+                return false;
+            if (map[idx][c] == target)
+                return false;
+        }
+
+        // 3x3 체크
+        int startR = r / 3 * 3;
+        int startC = c / 3 * 3;
+
+        for (int i = startR; i < startR + 3; i++)
+            for (int j = startC; j < startC + 3; j++)
+                if (map[i][j] == target)
+                    return false;
+
+        return true;
+    }
+
+    static class Pair {
+        int x;
+        int y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+        read();
+        dfs(0);
+    }
+
+    private static void read() {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 9; ++i)
+            for (int j = 0; j < 9; ++j) {
+                map[i][j] = sc.nextInt();
+                if (map[i][j] == 0)
+                    pairs[len++] = new Pair(i, j);
+            }
+        sc.close();
+    }
+}
---
+++ Baekjoon/dfs/RedGreen.java
@@ -0,0 +1,78 @@
+package Baekjoon.dfs;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 적록색약
+public class RedGreen {
+    static char[][] map;
+    static boolean[][] visited;
+    static int lines;
+    static int cnt1 = 0;
+    static int cnt2 = 0;
+
+    public static void read() {
+        Scanner sc = new Scanner(System.in);
+        lines = sc.nextInt();
+        map = new char[lines][lines];
+        visited = new boolean[lines][lines];
+        for (int i = 0; i < lines; ++i) {
+            String s = sc.next();
+            map[i] = s.toCharArray();
+        }
+        sc.close();
+    }
+
+    private static void run() {
+        for (int r = 0; r < lines; ++r)
+            for (int c = 0; c < lines; ++c)
+                if (!visited[r][c]) {
+                    dfs(r, c, map[r][c]);
+                    cnt1++;
+                }
+        for (int i = 0; i < lines; ++i)
+            Arrays.fill(visited[i], false);
+        for (int r = 0; r < lines; ++r)
+            for (int c = 0; c < lines; ++c)
+                if (!visited[r][c]) {
+                    if (map[r][c] == 'R' || map[r][c] == 'G')
+                        dfs(r, c, 'R', 'G');
+                    else
+                        dfs(r, c, 'B');
+                    cnt2++;
+                }
+    }
+
+    private static void dfs(int r, int c, char... ch) {
+        int[] dr = { 1, -1, 0, 0 };
+        int[] dc = { 0, 0, -1, 1 };
+
+        visited[r][c] = true;
+        for (int i = 0; i < 4; ++i) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (nr < 0 || nc < 0 || nr >= lines || nc >= lines)
+                continue;
+            if (visited[nr][nc])
+                continue;
+            if (matches(map[nr][nc], ch)) {
+                visited[nr][nc] = true;
+                dfs(nr, nc, ch);
+            }
+        }
+    }
+
+    private static boolean matches(char c, char... ch) {
+        for (int i = 0; i < ch.length; ++i)
+            if (c == ch[i])
+                return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        read();
+        run();
+        System.out.println(cnt1 + " " + cnt2);
+    }
+}
